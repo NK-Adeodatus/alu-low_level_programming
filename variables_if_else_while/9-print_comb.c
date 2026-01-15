5a44a14fff7e6b18e0b9b2d16445abe43dd2b764
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-01-15 13:16:21 +0200

    Add program to print combinations of single digits
    Implement a program to print all possible combinations of single digits.

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints all possible combinations of single digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
