60cde3b7ad569eaf5537206b7a6d519636cf1e4f
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-02-06 20:23:48 +0200

    Update 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,11 @@
 
 /**
  * reset_to_98 - updates the value it points to to 98
+ * @n: pointer to an integer
  *
- * 
+ * Description: This function takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * Return: void
  */
 void reset_to_98(int *n)
 {
