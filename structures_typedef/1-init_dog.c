dbee35c0f85cbeae03136788e52f08316c03f698
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-02-28 15:05:12 +0200

    Refactor init_dog function and enhance comments
    Updated function documentation and improved null check.

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,20 +1,21 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to the struct dog to initialize
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
  *
- * Return: nothing
+ * Description: This function initializes a struct dog with the given values
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
