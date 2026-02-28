0e4d8c39795b39faa0d2c5c2875e40c75b381d82
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-02-28 15:10:29 +0200

    Add init_dog function prototype to dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - defines a dog
  * @name: name of the dog
@@ -13,5 +12,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
