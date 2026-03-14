7e60cba334bca65794e37e9dcb63284dc7cf81cd
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-03-14 18:59:47 +0200

    implement 3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <string.h>
+
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * @s: the operator passed as argument to the program
+ *
+ * Return: pointer to the function that corresponds to the operator
+ *         or NULL if operator doesn't match any of the 5 expected operators
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
